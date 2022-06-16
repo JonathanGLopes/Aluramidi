66f9f701ff956287cb709791aab56838d0dd0e49
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-06-16 19:15:43 -0300

    Finalizando projeto

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function tocaSom(idAudioTecla){
-    document.querySelector(idAudioTecla).play();
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
+
 }
 
 const $listaTeclas = document.querySelectorAll(".tecla");
